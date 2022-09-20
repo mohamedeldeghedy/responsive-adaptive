218ef6127d8673f0bb1210eb0330c6417f2ee0d1
mohamedeldeghedy <45645800+mohamedeldeghedy@users.noreply.github.com>
2022-09-20 14:47:40 +0200

    register from mohamed

--- lib/mobile.dart
+++ lib/mobile.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class MobileScreen extends StatelessWidget {
   const MobileScreen({Key? key}) : super(key: key);
 //ok that's good
+  //register
   @override
   Widget build(BuildContext context) {
     return Scaffold(
